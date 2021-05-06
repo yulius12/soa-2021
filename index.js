de07700877d085dcdd618b399bd410639b0a2e5f
yulius <yulius1620@gmail.com>
2021-05-06 14:43:29 +0700

    push kelima

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const port=process.env.PORT || 3000;
 
 app.get("/",function (req,res) {
-    return res.render("displaymenu",{type:"Indonesian",menu:["batagor","rujak","kluntung"]})
+    return res.send({type:"Indonesian",menu:["batagor","rujak","kluntung"]})
 })
 
 app.listen(port,function () {

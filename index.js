81ef2f122713d027dc8be07d1379c94f974368a6
yulius <yulius1620@gmail.com>
2021-05-06 15:18:19 +0700

    push_terakir

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const port=process.env.PORT||3000:
+const port=process.env.PORT || 3000:
 const app = express();
 app.set('view engine','ejs');
 const pengguna = require('./routes/pengguna');
